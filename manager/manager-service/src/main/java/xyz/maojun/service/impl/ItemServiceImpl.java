cb87040b718fcbcfb52b331d979f17a42e8295d8
oilmonkey <maojunxyz@gmail.com>
2018-03-06 20:42:40 +0800

    :zap:add item and itemDesc cache use redis

--- manager/manager-service/src/main/java/xyz/maojun/service/impl/ItemServiceImpl.java
+++ manager/manager-service/src/main/java/xyz/maojun/service/impl/ItemServiceImpl.java
@@ -2,14 +2,22 @@ package xyz.maojun.service.impl;
 
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
+import com.sun.org.apache.bcel.internal.generic.IREM;
+import javassist.bytecode.stackmap.BasicBlock;
 import org.apache.activemq.command.ActiveMQTopic;
+import org.apache.commons.lang3.StringUtils;
+import org.jboss.netty.util.internal.StringUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.jms.core.MessageCreator;
 import org.springframework.stereotype.Service;
+import xyz.maojun.common.jedis.JedisClient;
+import xyz.maojun.common.jedis.JedisClientPool;
 import xyz.maojun.common.pojo.EasyUIDateGridResult;
 import xyz.maojun.common.util.EgouResult;
 import xyz.maojun.common.util.IDUtils;
+import xyz.maojun.common.util.JsonUtils;
 import xyz.maojun.mapper.TbItemDescMapper;
 import xyz.maojun.mapper.TbItemMapper;
 import xyz.maojun.pojo.TbItem;
@@ -21,6 +29,7 @@ import javax.annotation.Resource;
 import javax.jms.*;
 import java.util.Date;
 import java.util.List;
+import java.util.PriorityQueue;
 
 @Service
 public class ItemServiceImpl implements ItemService {
@@ -33,10 +42,28 @@ public class ItemServiceImpl implements ItemService {
     private JmsTemplate jmsTemplate;
     @Resource
     private Destination topicDestination;
+    @Autowired
+    private JedisClient jedisClient;
 
+    @Value("${REDIS_ITEM_PRE}")
+    private String REDIS_ITEM_PRE;
+
+    @Value(("${ITEM_CACHE_EXPIRE}"))
+    private Integer ITEM_CACHE_EXPIRE;
 
     @Override
     public TbItem getItemById(long itemid) {
+        // 查询缓存
+        try {
+            String json = jedisClient.get(REDIS_ITEM_PRE + ":" + itemid + ":BASE");
+            if (StringUtils.isNotBlank(json)) {
+                TbItem tbItem = JsonUtils.jsonToPojo(json, TbItem.class);
+                return tbItem;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         // 根据主键查询
 //        TbItem item = tbItemMapper.selectByPrimaryKey(itemid);
 //        return item;
@@ -47,6 +74,13 @@ public class ItemServiceImpl implements ItemService {
         // 执行查询
         List<TbItem> list = tbItemMapper.selectByExample(example);
         if (list != null && list.size() > 0) {
+            // 添加到缓存
+            try {
+                jedisClient.set(REDIS_ITEM_PRE+":" + itemid + ":BASE", JsonUtils.objectToJson(list.get(0)));
+                jedisClient.expire(REDIS_ITEM_PRE+":" + itemid + ":BASE", ITEM_CACHE_EXPIRE);
+            } catch (Exception e) {
+               e.printStackTrace();
+            }
             return list.get(0);
         }
         return null;
@@ -92,5 +126,27 @@ public class ItemServiceImpl implements ItemService {
             }
         });
         return EgouResult.ok();
+    }
+
+    @Override
+    public TbItemDesc getItemDescById(long itemId) {
+        // 查询缓存
+        try {
+            String json = jedisClient.get(REDIS_ITEM_PRE + ":" + itemId + ":DESC");
+            if (StringUtils.isNotBlank(json)) {
+                TbItemDesc tbItemDesc= JsonUtils.jsonToPojo(json, TbItemDesc.class);
+                return tbItemDesc;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        TbItemDesc itemDesc = tbItemDescMapper.selectByPrimaryKey(itemId);
+        try {
+            jedisClient.set(REDIS_ITEM_PRE + ":" + itemId + ":DESC", JsonUtils.objectToJson(itemDesc));
+            jedisClient.expire(REDIS_ITEM_PRE+":" + itemId + ":DESC", ITEM_CACHE_EXPIRE);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return itemDesc;
     }
 }
