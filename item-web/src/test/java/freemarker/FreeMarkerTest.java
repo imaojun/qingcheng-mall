39373d7d19055ce76ac92e9cfabfcc0e60143920
oilmonkey <maojunxyz@gmail.com>
2018-03-06 21:33:02 +0800

    :white_check_mark:add test with pojo use freemarker

--- item-web/src/test/java/freemarker/FreeMarkerTest.java
+++ item-web/src/test/java/freemarker/FreeMarkerTest.java
@@ -19,10 +19,13 @@ public class FreeMarkerTest {
         Configuration configuration = new Configuration(Configuration.getVersion());
         configuration.setDirectoryForTemplateLoading(new File("E:\\egouMall\\item-web\\src\\main\\webapp\\WEB-INF\\ftl"));
         configuration.setDefaultEncoding("utf-8");
-        Template template = configuration.getTemplate("hello.ftl");
+//        Template template = configuration.getTemplate("hello.ftl");
+        Template template = configuration.getTemplate("student.ftl");
         Map data= new HashMap<>();
+        StudentTest student = new StudentTest(1, "jack", 12, "china");
+        data.put("student", student);
         data.put("hello", "hello freemarker!");
-        Writer out = new FileWriter(new File("E:\\frl.html"));
+        Writer out = new FileWriter(new File("E:\\student.html"));
         template.process(data, out);
         out.close();
 
