0fc15c59cc1009988c1ac7b1b7d0cccd88c15ac9
oilmonkey <maojunxyz@gmail.com>
2018-03-09 21:36:20 +0800

    :bento: Added login page callback order-web page function

--- sso-web/src/main/java/xyz/maojun/sso/controller/LoginController.java
+++ sso-web/src/main/java/xyz/maojun/sso/controller/LoginController.java
@@ -3,6 +3,7 @@ package xyz.maojun.sso.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -22,7 +23,8 @@ public class LoginController {
     private String TOKEN_KEY;
 
     @RequestMapping("/page/login")
-    public String showLogin() {
+    public String showLogin(String redirect ,Model model) {
+        model.addAttribute("redirect", redirect);
         return "login";
     }
 
